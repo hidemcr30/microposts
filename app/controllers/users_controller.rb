72d75d6a8cd2f86f3f6dcfefa80c5595ea862a1f
hidemcr30 <hidemcr30@gmail.com>
2015-10-20 13:19:25 +0000

    profile update action add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def show
    @user = User.find(params[:id])
   end
@@ -18,10 +18,25 @@ class UsersController < ApplicationController
     end
   end
     
+  def edit
+    @user = current_user
+  end
+    
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to current_user
+    else
+      render 'edit'
+    end
+  end
+  
+    
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :area, :profile, :email, :password, :password_confirmation)
   end
   
 end
